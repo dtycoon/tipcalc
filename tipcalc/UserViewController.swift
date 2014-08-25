75da957d7249645713b6d7e4b38323b36c43b645
dtycoon <deepak@Deepaks-MacBook-Pro.local>
2014-08-24 22:19:08 -0700

    correct files

--- tipcalc/UserViewController.swift
+++ tipcalc/UserViewController.swift
@@ -13,6 +13,17 @@ class UserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var defaults = NSUserDefaults.standardUserDefaults()
+        
+        var tip1 = defaults.integerForKey("tip_percertage_1")
+        tipField1.text = "\(tip1)"
+           var tip2 = defaults.integerForKey("tip_percertage_2")
+        tipField2.text = "\(tip2)"
+
+        var tip3 = defaults.integerForKey("tip_percertage_3")
+        tipField3.text = "\(tip3)"
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +32,10 @@ class UserViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var tipField3: UITextField!
+    @IBOutlet weak var tipField2: UITextField!
 
+    @IBOutlet weak var tipField1: UITextField!
     /*
     // MARK: - Navigation
 
@@ -32,4 +46,49 @@ class UserViewController: UIViewController {
     }
     */
 
+    @IBAction func onTap(sender: AnyObject) {
+         view.endEditing(true)
+    }
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+    var tip1:Int? = tipField1.text.toInt()
+        if (tip1 == nil)
+        {
+            tip1 = 0
+            tipField1.text = "0"
+            
+        }
+        else
+        {
+            tipField1.text = "\(tip1!)"
+        }
+    defaults.setInteger(tip1!, forKey: "tip_percertage_1")
+        
+    var tip2:Int? = tipField2.text.toInt()
+        if (tip2 == nil)
+        {
+            tip2 = 0
+            tipField2.text = "0"
+            
+        }
+        else
+        {
+        tipField2.text = "\(tip2!)"
+        }
+        defaults.setInteger(tip2!, forKey: "tip_percertage_2")
+        
+        var tip3:Int? = tipField3.text.toInt()
+        if (tip3 == nil)
+        {
+            tip3 = 0
+            tipField3.text = "0"
+            
+        }
+        else
+        {
+        tipField3.text = "\(tip3!)"
+        }
+        defaults.setInteger(tip3!, forKey: "tip_percertage_3")
+ 
+    }
 }
--- tipcalc/ViewController.swift
+++ tipcalc/ViewController.swift
@@ -9,17 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    var tipPercentages: [Double] = [0, 0, 0]
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        tipLabel.text="$0.00"
+        totalLabel.text="$0.00"
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+        
+        var tip1 = defaults.integerForKey("tip_percertage_1")
+        tipPercentages[0] = (tip1 as NSNumber).doubleValue / 100
+       tipControl.setTitle("\(tip1)%", forSegmentAtIndex: 0)
+        
+        var tip2 = defaults.integerForKey("tip_percertage_2")
+        tipPercentages[1] = (tip2 as NSNumber).doubleValue / 100
+        tipControl.setTitle("\(tip2)%", forSegmentAtIndex: 1)
+        
+        var tip3 = defaults.integerForKey("tip_percertage_3")
+        tipPercentages[2] = (tip3 as NSNumber).doubleValue / 100
+       tipControl.setTitle("\(tip2)%", forSegmentAtIndex: 2)
 
+    }
+    @IBOutlet weak var tipLabel: UILabel!
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        println("view will appear")
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        println("view did appear")
+    }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        println("view will disappear")
+    }
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        println("view did disappear")
+    }
 }
 
