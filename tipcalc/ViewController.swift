a84cf907f17f50ec6ecb84ec459186938be5f7b2
dtycoon <deepak@Deepaks-MacBook-Pro.local>
2014-08-24 23:27:31 -0700

    making code more modular

--- tipcalc/ViewController.swift
+++ tipcalc/ViewController.swift
@@ -19,20 +19,7 @@ class ViewController: UIViewController {
         tipLabel.text="$0.00"
         totalLabel.text="$0.00"
         
-        var defaults = NSUserDefaults.standardUserDefaults()
         
-        var tip1 = defaults.integerForKey("tip_percertage_1")
-        tipPercentages[0] = (tip1 as NSNumber).doubleValue / 100
-       tipControl.setTitle("\(tip1)%", forSegmentAtIndex: 0)
-        
-        var tip2 = defaults.integerForKey("tip_percertage_2")
-        tipPercentages[1] = (tip2 as NSNumber).doubleValue / 100
-        tipControl.setTitle("\(tip2)%", forSegmentAtIndex: 1)
-        
-        var tip3 = defaults.integerForKey("tip_percertage_3")
-        tipPercentages[2] = (tip3 as NSNumber).doubleValue / 100
-       tipControl.setTitle("\(tip3)%", forSegmentAtIndex: 2)
-
     }
     @IBOutlet weak var tipLabel: UILabel!
 
@@ -48,17 +35,43 @@ class ViewController: UIViewController {
 
     @IBAction func onEditingChanged(sender: AnyObject) {
 
+        loadTotal()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        println("view will appear")
+        loadTipControl()
+        loadTotal()
+
+    }
+    func loadTipControl()
+    {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        
+        var tip1 = defaults.integerForKey("tip_percertage_1")
+        tipPercentages[0] = (tip1 as NSNumber).doubleValue / 100
+        tipControl.setTitle("\(tip1)%", forSegmentAtIndex: 0)
+        
+        var tip2 = defaults.integerForKey("tip_percertage_2")
+        tipPercentages[1] = (tip2 as NSNumber).doubleValue / 100
+        tipControl.setTitle("\(tip2)%", forSegmentAtIndex: 1)
+        
+        var tip3 = defaults.integerForKey("tip_percertage_3")
+        tipPercentages[2] = (tip3 as NSNumber).doubleValue / 100
+        tipControl.setTitle("\(tip3)%", forSegmentAtIndex: 2)
+
+    }
+    
+    func loadTotal()
+    {
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         var billAmount = (billField.text as NSString).doubleValue
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        println("view will appear")
+
     }
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
